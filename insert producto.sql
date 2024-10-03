4f575d09180900bee9412253ff46197caa1b0a73
Joseph Romero <88736577+josephypr@users.noreply.github.com>
2024-10-03 13:47:16 -0500

    Add files via upload

---
+++ insert producto.sql
@@ -0,0 +1,4 @@
+SELECT * FROM videojuegos.producto;
+insert into videojuegos.producto (idproducto,nombre,tipo,precio, descripcion) values (01, 'halo', 'videojuego', '50000','juego violento' );
+insert into videojuegos.producto (idproducto,nombre,tipo,precio, descripcion) values (02, 'control', 'electrodomestico', '200000','control para nintendo switch' );
+insert into videojuegos.producto (idproducto,nombre,tipo,precio, descripcion) values (03, 'camisa', 'ropa', '60000','camiseta de mario' );
---
+++ insert usuario.sql
@@ -0,0 +1,4 @@
+SELECT * FROM videojuegos.usuario;
+insert into videojuegos.usuario (idusuario,nombre,direccion,pais) values (01, 'camilo', 'cr08', 'colombia' );
+insert into videojuegos.usuario (idusuario,nombre,direccion,pais) values (02, 'andrey', 'cr12', 'colombia' );
+insert into videojuegos.usuario (idusuario,nombre,direccion,pais) values (03, 'joseph', 'cr04', 'colombia' );
