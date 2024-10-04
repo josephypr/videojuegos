79b272afdf6a9107c9e97e86d21123bbc129fc9e
Joseph Romero <88736577+josephypr@users.noreply.github.com>
2024-10-04 12:07:35 -0500

    Delete insert usuario.sql

--- insert usuario.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM videojuegos.usuario;
-insert into videojuegos.usuario (idusuario,nombre,direccion,pais) values (01, 'camilo', 'cr08', 'colombia' );
-insert into videojuegos.usuario (idusuario,nombre,direccion,pais) values (02, 'andrey', 'cr12', 'colombia' );
-insert into videojuegos.usuario (idusuario,nombre,direccion,pais) values (03, 'joseph', 'cr04', 'colombia' );
