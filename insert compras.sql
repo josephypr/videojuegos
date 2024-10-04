20eb26c5548a2992681533cbaf7639c59cc12ceb
Joseph Romero <88736577+josephypr@users.noreply.github.com>
2024-10-04 12:06:59 -0500

    Delete insert compras.sql

--- insert compras.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM videojuegos.compra;
-insert into videojuegos.compra (idcompra, fecha, precio_total, metodo_pago, usuario_idusuario) values (01,'2024-10-04', '50000', 'tarjeta', '01');
-insert into videojuegos.compra (idcompra, fecha, precio_total, metodo_pago, usuario_idusuario) values (02,'2024-11-10', '200000', 'tarjeta', '02');
-insert into videojuegos.compra (idcompra, fecha, precio_total, metodo_pago, usuario_idusuario) values (03,'2024-12-01', '60000', 'tarjeta', '03');
