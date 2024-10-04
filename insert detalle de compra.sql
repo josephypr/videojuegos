c533044c20a3a63a01f352b4dd0e0e82e8244796
Joseph Romero <88736577+josephypr@users.noreply.github.com>
2024-10-04 12:07:11 -0500

    Delete insert detalle de compra.sql

--- insert detalle de compra.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM videojuegos.`detalle de compra`;
-insert into videojuegos.`detalle de compra` (cantidad, compra_idcompra, compra_usuario_idusuario, producto_idproducto ) values (01,01,01, '01');
-insert into videojuegos.`detalle de compra` (cantidad, compra_idcompra, compra_usuario_idusuario, producto_idproducto ) values (02,02,02, 02);
-insert into videojuegos.`detalle de compra` (cantidad, compra_idcompra, compra_usuario_idusuario, producto_idproducto ) values (03,03,03, 03);
